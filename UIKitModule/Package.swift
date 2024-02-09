8b8666d60822847f211ec24e951ff3ae733e3c5c
Felipe Barusso <felipe.barusso@mblabs.com.br>
2024-02-09 16:38:34 -0300

    Removed unused package

--- UIKitModule/Package.swift
+++ UIKitModule/Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["UIKitModule"]),
     ],
     dependencies: [
-            .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
-            .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0")
+            .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0"))
         ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "UIKitModule",
-            dependencies: ["Kingfisher", "SkeletonView"],
+            dependencies: ["Kingfisher"],
             resources: [.process("Assets/Fonts")]),
         .testTarget(
             name: "UIKitModuleTests",
